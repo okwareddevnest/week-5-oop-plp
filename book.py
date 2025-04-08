7e534ca56884971607b989b32b25aa9d2c65d216
okwareddevnest <softengdedan@gmail.com>
2025-04-08 16:55:18 +0300

    Add rating functionality to Book class and implement main library interface

--- book.py
+++ book.py
@@ -7,6 +7,7 @@ class Book:
         self.author = author
         self.year = year
         self.available = True  #Default to available
+        self.__rating = 0 
 
     def borrow(self):
         """
@@ -28,7 +29,23 @@ class Book:
         """
         Returns formatted book information.
         """
-        return f"Hello here is {self.title} by {self.author} {self.year}"
+        return f"{self.title} by {self.author} {self.year}"
+    
+    def set_rating(self, rating):
+        """
+        Set a rating from 0 to 5 for the book.
+        This is an example of encapsulation: rating is private
+        """
+        if 0 <= rating <= 5:
+            self.__rating = rating
+        else:
+            print("Rating must be between 0 and 5")
+
+    def get_rating(self):
+        """
+        Get the current ratings of the book
+        """
+        return self.__rating
     
 
     # Book.get_details()
