eaded1afd16f7e97bdba190fea57fd6bbee4ecde
okwareddevnest <softengdedan@gmail.com>
2025-04-08 16:26:58 +0300

    Add DigitalBook and Magazine classes with borrowing behavior

---
+++ digitalbook.py
@@ -0,0 +1,19 @@
+from book import Book
+
+class DigitalBook(Book):
+    def __init__(self, title, author, year, file_size):
+        # Using super() to inherit the attributes from the Book class
+        super().__init__(title, author, year)
+        self.file_size = file_size
+
+    def stream(self):
+        """
+        Simulates streaming pf the digital book
+        """
+        return f"Streaming {self.title} - Size: {self.file_size}MB"
+    
+    def borrow(self):
+        """
+        Overrides the borrow method to reflect digital availability
+        """
+        return f"{self.title} is a digital book. No need to borrow - stream in anytime"
